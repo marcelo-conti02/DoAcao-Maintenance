b464269e3bcb5659e733e3d1c5d52e8c98a257a0
marcelo <marceloconti9017@gmail.com>
2024-07-02 21:18:26 -0300

    Add city filter to home page

--- doacao-frontend/src/screens/Home/index.jsx
+++ doacao-frontend/src/screens/Home/index.jsx
@@ -1,91 +1,101 @@
-import React, { useEffect, useState, useCallback } from 'react'
-
-import { Input, Tabs } from '../../components'
+import React, { useEffect, useState, useCallback } from 'react';
+import { Input, Tabs } from '../../components';
 import { useOrderService, useInstitutionService } from '../../services';
-import { OrdersListGroup } from './partials'
-import { MOCK_PRODUCT_ORDERS, MOCK_URGENT_PRODUCT_ORDERS, MOCK_SERVICE_ORDERS, MOCK_URGENT_SERVICE_ORDERS } from './mock'
-import { HomeScreen, FiltersContainers } from './styles'
+import { OrdersListGroup } from './partials';
+import { HomeScreen, FiltersContainers } from './styles';
 
 const Home = () => {
-    const { getPublicProductOrders, getPublicServiceOrders } = useOrderService()
-    const { getCities } = useInstitutionService();
-    const [tabsConfig, setTabsConfig] = useState([])
-    const [regularProductOrders, setRegularProductOrders] = useState([])
-    const [urgentProductOrders, setUrgentProductOrders] = useState([])
-    const [regularServicesOrders, setRegularServicesOrders] = useState([])
-    const [urgentServicesOrders, setUrgentServicesOrders] = useState([])
-    const [showContent, setShowContent] = useState(false)
-    const [cities, setCities] = useState([]);
+  const { getPublicProductOrders, getPublicServiceOrders, getCityByOrderId } = useOrderService();
+  const { getCities } = useInstitutionService();
+  const [tabsConfig, setTabsConfig] = useState([]);
+  const [regularProductOrders, setRegularProductOrders] = useState([]);
+  const [urgentProductOrders, setUrgentProductOrders] = useState([]);
+  const [regularServicesOrders, setRegularServicesOrders] = useState([]);
+  const [urgentServicesOrders, setUrgentServicesOrders] = useState([]);
+  const [showContent, setShowContent] = useState(false);
+  const [cities, setCities] = useState([]);
+  const [selectedCity, setSelectedCity] = useState('');
 
-    const getProductOrders = async () => {
-        const { data } = await getPublicProductOrders()
+  const getProductOrders = async () => {
+    const { data } = await getPublicProductOrders();
+    const filteredData = data.filter(({ orderId }) => !selectedCity || getCityByOrderId(orderId) === selectedCity);
 
-        const urgentOrders = data.filter(({ isUrgent }) => isUrgent)
-        const regularOrders = data.filter(({ isUrgent }) => !isUrgent)
+    const urgentOrders = filteredData.filter(({ isUrgent }) => isUrgent);
+    const regularOrders = filteredData.filter(({ isUrgent }) => !isUrgent);
+    
+    setRegularProductOrders(regularOrders);
+    setUrgentProductOrders(urgentOrders);
+  };
 
-        setRegularProductOrders(regularOrders)
-        setUrgentProductOrders(urgentOrders)
-    }
+  const getServiceOrders = async () => {
+    const { data } = await getPublicServiceOrders();
+    const filteredData = data.filter(order => !selectedCity || order.city === selectedCity);
 
-    const getServiceOrders = async () => {
-        const { data } = await getPublicServiceOrders()
-        const urgentOrders = data.filter(({ isUrgent }) => isUrgent)
-        const regularOrders = data.filter(({ isUrgent }) => !isUrgent)
+    const urgentOrders = filteredData.filter(({ isUrgent }) => isUrgent);
+    const regularOrders = filteredData.filter(({ isUrgent }) => !isUrgent);
 
-        setRegularServicesOrders(regularOrders)
-        setUrgentServicesOrders(urgentOrders)
-    }
+    setRegularServicesOrders(regularOrders);
+    setUrgentServicesOrders(urgentOrders);
+  };
 
-    const getData = useCallback(async () => {
-        Promise.all([
-            getProductOrders(),
-            getServiceOrders(),
-        ]).finally(() => setShowContent(true))
-    }, [])
+  const getData = useCallback(async () => {
+    Promise.all([
+      getProductOrders(),
+      getServiceOrders(),
+    ]).finally(() => setShowContent(true));
+  }, [selectedCity]);
 
-    const loadCities = async () => {
-        const citiesData = await getCities();
-        setCities(citiesData);
-      }
+  const loadCities = async () => {
+    const citiesData = await getCities();
+    setCities(citiesData);
+  };
 
-    useEffect(() => {
-        getData()
-    }, [getData])
+  useEffect(() => {
+    getData();
+  }, [getData]);
 
-    useEffect(() => {
-        loadCities();
-      }, [])
+  useEffect(() => {
+    loadCities();
+  }, []);
 
-    useEffect(() => {
-        setTabsConfig(
-            [
-                {
-                    children: <OrdersListGroup orders={regularProductOrders} urgentOrders={urgentProductOrders} />,
-                    title: 'Produtos',
-                },
-                {
-                    children: <OrdersListGroup orders={regularServicesOrders} urgentOrders={urgentServicesOrders} />,
-                    title: 'Voluntários',
-                },
-            ]
-        )
-    }, [regularProductOrders,
-        urgentProductOrders,
-        regularServicesOrders,
-        urgentServicesOrders])
+  useEffect(() => {
+    setTabsConfig(
+      [
+        {
+          children: <OrdersListGroup orders={regularProductOrders} urgentOrders={urgentProductOrders} />,
+          title: 'Produtos',
+        },
+        {
+          children: <OrdersListGroup orders={regularServicesOrders} urgentOrders={urgentServicesOrders} />,
+          title: 'Voluntários',
+        },
+      ]
+    );
+  }, [regularProductOrders, urgentProductOrders, regularServicesOrders, urgentServicesOrders]);
 
-    return (
-        <HomeScreen isPublic={true}>
-            {showContent &&
-                (
-                    <>
-                        <FiltersContainers>
-                        </FiltersContainers>
-                        <Tabs tabsConfigList={tabsConfig} />
-                    </>
-                )}
-        </HomeScreen>
-    )
-}
+  const handleCityChange = (event) => {
+    setSelectedCity(event.target.value);
+  };
 
-export { Home }
+  return (
+    <HomeScreen isPublic={true}>
+      {showContent && (
+        <>
+          <FiltersContainers>
+            <select onChange={handleCityChange} value={selectedCity}>
+              <option value="">Todas as cidades</option>
+              {cities.map((city, index) => (
+                <option key={index} value={city}>
+                  {city}
+                </option>
+              ))}
+            </select>
+          </FiltersContainers>
+          <Tabs tabsConfigList={tabsConfig} />
+        </>
+      )}
+    </HomeScreen>
+  );
+};
+
+export { Home };
